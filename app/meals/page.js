fb093e8c18728449664c795928794b98a3b85efb
O-Alidzhikov <oki30g@gmail.com>
2025-10-04 18:31:06 +0300

    Added some page content and styles

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,21 @@
-export default function Meals(){
-    return ( 
-        <div>
-            <p>This is the meals page!</p>
-        </div>
-    )
+import Link from "next/link";
+import classes from "./page.module.css";
+
+export default function Meals() {
+  return (
+    <>
+      <header>
+        <h1>
+          {" "}
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you!</span>
+        </h1>
+        <p>some text in a paragraph very texty and it should be long</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">share some cool recipes</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 }

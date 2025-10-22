a885e9c02ec72fce746145aec32060bdc4774860
O-Alidzhikov <oki30g@gmail.com>
2025-10-23 00:53:32 +0300

    the details page of each meal is populated with the corresponding data

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,3 +1,36 @@
-export default function customPage(){
-    return <h1> you can come here with any value!</h1>
+import Image from "next/image";
+import { getOne } from "@/app/services/meals-service";
+import classes from "./page.module.css";
+import { notFound } from "next/navigation";
+
+export default function customPage({ params }) {
+  const meal = getOne(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+  console.log(meal);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill></Image>
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
--- app/services/meals-service.js
+++ app/services/meals-service.js
@@ -5,3 +5,9 @@ const db = sql("meals.db")
 export function getAll(){
     return db.prepare("SELECT * FROM meals").all()
 }
+
+
+
+export function getOne(slug){
+    return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug)
+}
